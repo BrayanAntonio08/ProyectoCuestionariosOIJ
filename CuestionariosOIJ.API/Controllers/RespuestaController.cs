b1ad485eeb2cf2a168a5604b068521cc2342c2fd
BrayanAntonio08 <103152474+BrayanAntonio08@users.noreply.github.com>
2023-10-10 23:17:09 -0600

    Update respuesta API

--- CuestionariosOIJ.API/Controllers/RespuestaController.cs
+++ CuestionariosOIJ.API/Controllers/RespuestaController.cs
@@ -1,6 +1,4 @@
 ﻿using Cuestionarios.Domain;
-using CuestionariosOIJ.AccesoDatos.Context;
-using CuestionariosOIJ.AccesoDatos.EntitiesAD;
 using CuestionariosOIJ.ReglasNegocio;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -32,7 +30,7 @@ namespace CuestionariosOIJ.API.Controllers
         public async Task<ActionResult<List<Respuesta>>> ListarRespuestasTotales([FromBody] Cuestionario cuestionario)
         {
             RespuestaRN business = new RespuestaRN();
-            List<Respuesta> respuestas = business.ListarRespuestasTotales(new CuestionarioData(new CuestionariosContext()).ObtenerPorID(cuestionario.Id));
+            List<Respuesta> respuestas = business.ListarRespuestasTotales(cuestionario);
 
             return await Task.FromResult(Ok(respuestas));
         }
@@ -42,7 +40,7 @@ namespace CuestionariosOIJ.API.Controllers
         public async Task<ActionResult<List<Respuesta>>> ListarRespuestasPorPregunta([FromBody] Pregunta pregunta)
         {
             RespuestaRN business = new RespuestaRN();
-            List<Respuesta> respuestas = business.ListarRespuestasPorPregunta(new PreguntaData(new CuestionariosContext()).ObtenerPreguntaPorID(pregunta.Id));
+            List<Respuesta> respuestas = business.ListarRespuestasPorPregunta(pregunta);
 
             return await Task.FromResult(Ok(respuestas));
         }
@@ -51,7 +49,8 @@ namespace CuestionariosOIJ.API.Controllers
         public async Task<ActionResult<List<Respuesta>>> ListarRespuestasTotalesPorPregunta([FromBody] Pregunta pregunta)
         {
             RespuestaRN business = new RespuestaRN();
-            List<Respuesta> respuestas = business.ListarRespuestasTotalesPorPregunta(new PreguntaData(new CuestionariosContext()).ObtenerPreguntaPorID(pregunta.Id));
+            
+            List<Respuesta> respuestas = business.ListarRespuestasTotalesPorPregunta(pregunta);
 
             return await Task.FromResult(Ok(respuestas));
         }
